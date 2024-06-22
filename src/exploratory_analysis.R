e29130cc2d1555874d81df5d14d5453725ed0f6c
Steffen Aasen <98240487+a-asen@users.noreply.github.com>
2024-06-22 15:12:17 +0200

    Update meditation & music - Significant improvement in code.   - Table and figure added with more than 50% reduction in code.

--- src/exploratory_analysis.R
+++ src/exploratory_analysis.R
@@ -1,46 +1,7 @@
 library(ProjectTemplate)
 load.project()
 
-library(tidyverse)
-library(gt)
-
-
-q_t.test <- function(var, group, name = NULL){
-  if(is.null(name)){
-    "group_0" -> g_name
-    "group_1" -> g_name2
-  } else {
-    paste0(name, "_0") -> g_name
-    paste0(name, "_1") -> g_name2
-  }
-  
-  c(
-    set_names(mean(var[group==0]), g_name),
-    set_names(sd(var[group==0]), g_name),
-    set_names(mean(var[group==1]), g_name2),
-    set_names(sd(var[group==1]), g_name2),
-    set_names(mean(var[group==0] - var[group==1]), "mean_diff"),
-    #set_names(sd(var[group==0] - var[group==1]), "sd_diff"),
-    set_names(t.test(var[group==0], var[group==1])$parameter, "df"),
-    set_names(t.test(var[group==0], var[group==1])$statistic, "t,val"),
-    set_names(t.test(var[group==0], var[group==1])$p.value, "p.val"))
-}
-
-q_t.names <- function(name){
-  c(paste0(name, "_0_mean"), paste0(name, "_0_sd"), 
-    paste0(name, "_1_mean"), paste0(name, "_1_sd"), "mean_diff", #"sd_diff",
-    "df", "t.val", "p.val")
-}
-# q_t.test(pfc$probe1_n, pfc$meditation1)
-
-
-# library(brms)
-# testing_M <- brm(probe1 ~ zlogapen*zlogbv*stimulation + block*stimulation, 
-#                  pfc, family=cumulative(link = "probit"), init=0, backend = "cmdstanr",
-#     cores = 6, chains=6, iter = 4000 )
-# bayes_plot(testing_M)
-
-
+# Exploratory:            ======
 # Reverse the probes (higher value correspond to the probe Q)
 d.pro.stim_pfc |> 
   mutate(
@@ -54,194 +15,75 @@ d.pro.stim_pfc |>
     stimulation = factor(stimulation, levels = c("sham", "real")),
     meditation1 = ifelse(meditation>0, 1, 0), #as.integer(meditation),
     music_year1 = ifelse(music_year>0, 1, 0), 
-    music_year = ifelse(music_year>0, 1, 0), 
   ) -> pfc
 
 
-# Exploratory:            ======
-##   Meditation          =====
-med_data <- pfc |> 
-  summarize(
-    .by = c(subj, meditation1),
-    mw = mean(probe1_n),
-    ae = mean(zlogapen),
-    bv = mean(zlogbv)) |> 
-  reframe(
-    names = q_t.names("meditation"),
-    MW = q_t.test(mw, meditation1),
-    BV = q_t.test(bv, meditation1),
-    AE = q_t.test(ae, meditation1)
-  ) |>
-  pivot_longer(c(MW,BV,AE)) |>
-  pivot_wider(names_from = names) |>
-  mutate(p.adj = p.adjust(p.val, "bonferroni"))
-
-### Table        =====
-med_data
-  gt() |> 
-  fmt_number() |>
-  tab_spanner("No experience", c(	meditation_0_mean, 	meditation_0_sd) )  |>
-  tab_spanner("Any experience", c(	meditation_1_mean, 	meditation_1_sd) ) |>
-  cols_label(
-    ends_with("_mean") ~ md("*M*"),
-    ends_with("_sd") ~ md("*SD*"),
-    mean_diff = "Mdiff", df = md("*df*"),
-    t.val = md("*t*"), p.adj = md("*p*"), name = "" ) |>
-  cols_hide(p.val) |>
-  tab_footnote(md("*Note*. Bonferroni correct for three comparisons.")) |>
-  tab_header("Meditation experience on mind wandering (MW), behavioural variability (BV) and approximate entropy (AE)", 
-             ) |>
-  tab_options(heading.align = "left", 
-              table.font.names = "Times New Roman"
-              # ) |>  gtsave("report/tables/meditational_experience-mw+bv+ae.docx")
-)
-  
-###    Plot      ======
-p_meditation <-
+##  Music & Meditation        =====
+# get data
+m_m_data <- 
   pfc |> 
-  mutate(probe1_n = scale(probe1_n)) |> 
-  group_by(subj, meditation1) |>
-  summarize(MW = mean(probe1_n),
-            AE = mean(zlogapen),
-            BV = mean(zlogbv)) |> #pull(mw)
-  ungroup() |> 
-  pivot_longer(c(MW,AE,BV)) |>
-  mutate(name = factor(name, levels = c("MW", "BV", "AE"))) |>
-  ggplot(aes(factor(meditation1), value, col=factor(meditation1)))+
-  geom_hline(yintercept=0, linetype="dashed")+
-  facet_wrap(~name)+
-  #geom_point(position = position_nudge(x=-.1), alpha=.3)+
-  stat_summary()+
-  labs(x="Meditation experience",y="Z-scored difference", title="a")+
-  theme(legend.position="none")
-p_meditation
-#ggsave("figs/exploratory/meditation_experience.svg", p_meditation, width=6, height=3)
-
-# pfc |> 
-#   mutate(probe1_n = scale(probe1_n)) |> 
-#   group_by(subj, block, meditation1) |>
-#   summarize(MW = mean(probe1_n),
-#             AE = mean(zlogapen),
-#             BV = mean(zlogbv)) |> #pull(mw)
-#   ungroup() |> 
-#   pivot_longer(c(MW,AE,BV)) |>
-#   mutate(name = factor(name, levels = c("MW", "BV", "AE"))) |>
-#   ggplot(aes(block, value, col=factor(meditation1)))+
-#   facet_wrap(~name)+
-#   geom_point(position = position_nudge(x=-.1), alpha=.3)+
-#   stat_summary()+
-#   labs(x="Meditation experience",y="Z-scored difference")+
-#   theme(legend.position="none")
-
-
-##   Music experience        ======
-### Calculate           =====
-pfc |> 
-  summarize(
-    .by = c(subj, music_year1),
+  pivot_longer(c(meditation1, music_year1), names_to = "cat", values_to="cat_val") |>
+  mutate(probe1_n = scale(probe1_n)) |>
+  summarise(
+    .by = c(subj, cat),
+    cat_v = unique(cat_val),
     mw = mean(probe1_n),
     ae = mean(zlogapen),
-    bv = mean(zlogbv)) |> 
-  reframe(
-    names = q_t.names("music"),
-    MW = q_t.test(mw, music_year1),
-    BV = q_t.test(bv, music_year1),
-    AE = q_t.test(ae, music_year1)
-  ) |>
-  pivot_longer(c(MW,BV,AE)) |>
-  pivot_wider(names_from = names) |>
-  mutate(p.adj = p.adjust(p.val, "bonferroni")) -> mus_data
-  
-### Table       =====
-mus_data |> 
-  gt() |> 
-  fmt_number() |>
-  tab_spanner("No experience", c(music_0_mean, music_0_sd) )  |>
-  tab_spanner("Any experience", c(music_1_mean, music_1_sd) ) |>
-  cols_label(
-    ends_with("_mean") ~ md("*M*"),
-    ends_with("_sd") ~ md("*SD*"),
-    mean_diff = md("*M*~diff~"), df = md("*df*"), 
-    t.val = md("*t*"), p.adj = md("*p*"), name = "" ) |>
-  cols_hide(p.val) |>
-  tab_footnote(md("*Note*. Bonferroni correct for three comparisons.")) |>
-  tab_header("Musical experience on mind wandering (MW) ", "two") |>
-  tab_options(heading.align = "left", 
-              table.font.names = "Times New Roman"
-             # ) |>  gtsave("report/tables/musical_experience-mw+bv+ae.docx")
-)
-  # No-sig
+    bv = mean(zlogbv)) |>
+  pivot_longer(c(mw,bv,ae))
 
-###  Plot      =====
-p_music <- pfc |> 
-  mutate(probe1_n = scale(probe1_n)) |> 
-  group_by(subj, music_year1) |>
-  summarize(MW = mean(probe1_n),
-            AE = mean(zlogapen),
-            BV = mean(zlogbv)) |> #pull(mw)
-  ungroup() |> 
-  pivot_longer(c(MW,AE,BV)) |>
-  mutate(name = factor(name, levels = c("MW", "BV", "AE"))) |>
-  ggplot(aes(factor(music_year1), value, col=factor(music_year1)))+
-  geom_hline(yintercept=0, linetype="dashed")+
-  facet_wrap(~name)+
-#  geom_point(position = position_nudge(x=-.1), alpha=.3)+
-  stat_summary()+
-  labs(x="Music experience",y="Z-scored difference", title="b")+
-  theme(legend.position="none")
-p_music
-# ggsave("figs/exploratory/music_experience.svg", p_music, width=6, height=3)
-
-
-
-## COMBINED     =====
-
-### Plots       =====
-library(patchwork)
-p_meditation+p_music+plot_layout(nrow=2)
-ggsave("figs/exploratory/meditation_music-MW+BV+AE.png", dpi=300, height = 6, width = 6)
-
-
-### Tables      =====
-med_data |> 
-  mutate(.before=1, over="meditation") |>
-  rename( 
-    mean_0 = meditation_0_mean, 
-    sd_0 = meditation_0_sd,
-    mean_1 = meditation_1_mean, 
-    sd_1 = meditation_1_sd,
-    ) |> bind_rows(
-      mus_data |> 
-        mutate(.before=1, over="music") |>
-        rename(
-          mean_0 = music_0_mean, 
-          sd_0 = music_0_sd,
-          mean_1 = music_1_mean, 
-          sd_1 = music_1_sd,
-        )
-    ) |>
+### Tabel     =====
+m_m_data |>
+  summarise(
+    .by = c(cat, name), 
+    m0_m  = mean(value[cat_v==0]),
+    m0_sd = sd(value[cat_v==0]),
+    m1_m  = mean(value[cat_v==1]),
+    m1_sd = sd(value[cat_v==1]),
+    mdiff = mean(value[cat_v==1]) - mean(value[cat_v==0]),
+    t  = t.test(value[cat_v==1], value[cat_v==0])$statistic, 
+    df = t.test(value[cat_v==1], value[cat_v==0])$parameter, 
+    p  = t.test(value[cat_v==1], value[cat_v==0])$p.value, 
+    bf = 1/extractBF( ttestBF(value[cat_v==1], value[cat_v==0]) )$bf
+  ) |> 
+  mutate(.by = cat, p.adj = p.adjust(p, "bonferroni")) |> 
   mutate(
-    across(contains("p."), ~fmt_APA_numbers(.x, .p=T)),
-    across(c(df, t.val, contains("_")), ~fmt_APA_numbers(.x))
+    across(contains("p"), ~fmt_APA_numbers(.x,.p=T)),
+    across(where(is.double), ~fmt_APA_numbers(.x)),
+    name = fct_recode(name, MW="mw", BV="bv", AE="ae"),
+    cat = fct_recode(cat, Meditation="meditation1", Music="music_year1"),
+    e="", e2="", 
   ) |>
-  gt() |>
-  tab_spanner("No experience", c(mean_0, sd_0) )  |>
-  tab_spanner("Any experience", c(mean_1, sd_1) ) |>
+  rename(Variable="name") |>
+  gt(groupname_col = "cat") |>
+  tab_spanner("No", starts_with("m0_")) |>
+  tab_spanner("Any", starts_with("m1_")) |>
+  tab_spanner("Experience", c(matches("m[01]"),"e")) |>
   cols_label(
-    starts_with("mean") ~ md("*M*"),
-    starts_with("sd") ~ md("*SD*"),
-    mean_diff = md("*M*~diff~"),
-    df = md("*df*"), t.val = md("*t*"), p.val = md("*p*"),
-    p.adj = md("*p*~adj~")
+    ends_with("_m") ~ md("*M*"),
+    ends_with("_sd") ~ md("*SD*"),
+    t=md("*t*"), df = md("*df*"), p=md("*p*"), p.adj = md("*p*~adj~"), 
+    bf = md("BF~01~"), mdiff=md("*M*~diff~"),
+    starts_with("e")~""
   ) |>
-  tab_header("Meditation and musical experience on mind wandering (MW), behavioural variability (BV) and approximate entropy (AE)"
-             ) |>
-  tab_footnote(md("*Note*. *p*~adj~ is Bonferroni correct for three comparisons.")) |>
-  tab_options(heading.align = "left", 
-              table.font.names = "Times New Roman"
-              # ) |>  gtsave("report/tables/musical_experience-mw+bv+ae.docx")
+  cols_move(p.adj, p) |>
+  cols_move(e, m0_sd) |>
+  cols_move(e2, m1_sd) 
+  
+### Figure      =====
+m_m_data |>
+  mutate(
+    cat_v = factor(cat_v, label=c("No", "Any")),
+    cat = ifelse(cat=="meditation1", "Meditation", "Music"),
+    name = fct_recode(name, AE="ae", BV="bv", MW="mw") |> 
+      fct_relevel("MW","BV","AE")
   ) |>
-  gtsave("tables/meditation_and_musical_experience.docx")
+  rename(Experience = cat_v) |>
+  ggplot(aes(Experience, value, col=Experience)) + 
+  facet_wrap(cat~name) +
+  stat_summary(fun.data=mean_se) + 
+  geom_hline(yintercept=0, linetype="dashed")
+
 
 ## Test accumulating effects of TMS      =====
 pfc |>
