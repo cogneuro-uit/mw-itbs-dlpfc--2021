57e0e1b980617019b4c5b00ffcb8da16be177c9f
Steffen Aasen <github.plop432@slmail.me>
2025-04-22 09:28:10 +0200

    Update fmt_APA

--- lib/fmt_APA.R
+++ lib/fmt_APA.R
@@ -1,99 +1,104 @@
 # Format numbers to APA standard
-fmt_APA_numbers <- function(num, .p = FALSE, .psym = FALSE, .low_val = FALSE, .chr = FALSE){
+fmt_APA_numbers <- function(x, .p = FALSE, .psym = FALSE, .low_val = FALSE, .chr = FALSE, .rm_leading_0 = FALSE){
   require(purrr)
   
-  purrr::map(num, \(num){
-    o_num <- num
+  purrr::map(x, \(num){
     # Store original value
+    o_num <- num
+    
+    # Force to numeric, otherwise return original
     num <- as.numeric(num)
-    
-    # Transform to numeric (if possible)
-    if(is.na(num)){
+    if( is.na(num) ){
+      # if is not numeric after transformation, return the original 
       return(o_num)
-      # if is not numeric after transformation, return original
     }
     
-    # p with symbol
+    # Frequentist PROBABILITY (p) with SYMBOLS
     # = symbol:
-    if(.psym){
-      if(num < .001){
-        return("< .001")
-      } else {
-        return( round(num, 3) |> as.character() |> str_replace("0.", "= ") )
+    if(.psym & !.p){
+      if( num > .999 ){ # for some reason we do not have "1"
+        return("> .999")
       }
+      if( num < .001 ){
+        return("< .001")
+      } 
+      return( round(num, 3) |> as.character() |> str_replace("0.", "= .") )
     }
     
-    # < than symbol
+    # Frequentist PROBABILITY (p) 
     if(.p){
-      if( num == 1 ){
-        return( "1.00" )
-      }
+      if( num > .999 ) return( ".999")
+      if( num < .001 ) return( ".001") 
       
-      num <- round(num, 3)
-      if( num == 1 ){
-        return( "> .999")
-      }
+      # else...
+      num <- 
+        num |> 
+        round(digits = 3) |> 
+        as.character() |> 
+        str_replace("0.", ".")
       
-      if(num < .001){ 
-        num <- "< .001"
-      } else {
-        num <- num |> as.character() |> str_replace("0.", ".")
-        if(str_length(num) <= 3){
-          for(x in 1:(4 - str_length( num)) ){
-            num <- paste0(num, "0")
-          }
+      if( str_length(num) <= 3){
+        for(x in 1:(4 - str_length( num )) ){
+          num <- paste0(num, "0")
         }
       }
       return( num ) 
     }
     
-    # > 100
-    if(num >= 100 | num <= -100){
-      num <- round(num, 0)
+    # At 100, no preceding 0 (e.g., 153 instead of 153.382)
+    if( num >= 100 | num <= -100 ){
+      num <- round( num, 0 ) 
       if(.chr){
-        return( as.character( num) ) 
-      }
+        num <- as.character( num )
+        if(.rm_leading_0) num <- str_remove(num, "^0")
+      } 
       return( num )
     }
     
-    # > 10
-    if(num >= 10 | num <= -10){
+    # At 10 or more, one preceeding 0 (e.g., 12.1 instead of 12.132)
+    if( num >= 10 | num <= -10 ){
       num <- round(num, 1)
+      
       # Add an extra 0 using strings to return tidy APA numbers 
       if(.chr){
         if(str_length( as.character( abs(num) ) ) == 2){
           num <- paste0(num, ".0") # if exactly two, always add .0)
         }
+        if(.rm_leading_0) num <- str_remove(num, "^0")
         return( as.character(num) )
       }
     }
     
-    # > 1  | > 1 & .low_val
-    if(num >= 1 | num <= -1 | num < 1 & !.low_val | num > -1 & !.low_val){
+    # At 1 or more 1, two preceding 0's (e.g., 4.29 instead of 4.293125)
+    # *Also with low-val 
+    if( num >= 1 | num <= -1 | 
+        num < 1 & !.low_val | num > -1 & !.low_val){
       num <- round(num, 2)
       
       if(.chr){
-        if(str_length( as.character( abs(num) ) ) <= 3){
+        if( str_length( as.character( abs(num) ) ) <= 3 ){
+
           # Add an extra 0 using strings to return tidy APA numbers 
           for(x in 1:(4 - str_length( as.character( abs(num) ) )) ){
-            if(str_detect(num,"\\.", negate = TRUE)){
+            if( str_detect(num,"\\.", negate = TRUE) ){
               num <- paste0(num, ".")
             } else{
               num <- paste0(num, "0")
             }
           }
         }
+        if(.rm_leading_0) num <- str_remove(num, "^0")
         return( as.character(num) )
       }
     }
     
     
-    # Low values 
+    # At values below 1, 3 preceding 0's (e.g., 0.234, instead of 0.234423)
     if(num < 1 & .low_val | num > -1 & .low_val){
       num <- round(num, 3)
       if(.chr){
         if(str_length( as.character( abs(num) ) ) <= 4){
-        # Add an extra 0 using strings to return tidy APA numbers 
+          # Add an extra 0 using strings to return tidy APA numbers 
           for(x in 1:(5 - str_length( as.character( abs(num) ) )) ){
             if(str_detect(num,"\\.", negate = TRUE)){
               num <- paste0(num, ".")
@@ -102,6 +107,7 @@ fmt_APA_numbers <- function(num, .p = FALSE, .psym = FALSE, .low_val = FALSE, .c
             }
           }
         }
+        if(.rm_leading_0) num <- str_remove(num, "^0")
         return( as.character(num) )
       }
     }
@@ -109,5 +115,52 @@ fmt_APA_numbers <- function(num, .p = FALSE, .psym = FALSE, .low_val = FALSE, .c
     return( num )
   }) |> unlist()
   
+}
+
+
+tab_fmt_APA <- function(data){
+  require(gt)
+  #' Supplementary function to quickly transform a GT table to an approxiate APA 7 format
+  #' 
   
+  tab_line_width <- 1
+  tab_line_colour <- "black"
+  
+  data |>
+    tab_style(cell_text(size = 12, align="left"), cells_title()) |>
+    tab_options(
+      # General TABLE params
+      table.background.color = "white",
+      table.border.top.color = "white",
+      table.border.bottom.color = "white",
+      table_body.border.bottom.color = tab_line_colour,
+      table.width = pct(100),
+      # Column label things
+      column_labels.border.top.width = tab_line_width, # +1 ?
+      column_labels.border.top.color = tab_line_colour,
+      column_labels.border.bottom.width = tab_line_width,
+      column_labels.border.bottom.color = tab_line_colour,
+      # ROW groups (if relevant)
+      row_group.border.bottom.color = tab_line_colour,
+      row_group.border.bottom.width = tab_line_width,
+      row_group.border.top.color = tab_line_colour,
+      row_group.border.top.width = tab_line_width,
+    ) |>
+    # set table style
+    tab_style(
+      style = list(
+        # remove horizontal lines
+        cell_borders(
+          sides = c("top", "bottom"),
+          color = "white",
+          weight = px(1)
+        ),
+        # remove row striping in Markdown documents
+        cell_fill(color = "white", alpha = NULL)
+      ),
+      # do this for all columns and rows
+      locations = cells_body(
+        columns = everything(),
+        rows = everything()
+      ))
 }
